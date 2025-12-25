31012804786914e285a2ee3925626053d65591db
stefano <30480337+snebo@users.noreply.github.com>
2025-12-25 01:35:52 +0100

    generate base components

---
+++ src/app/search-input/search-input.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-input',
+  imports: [],
+  templateUrl: './search-input.html',
+  styleUrl: './search-input.css',
+})
+export class SearchInput {
+
+}
