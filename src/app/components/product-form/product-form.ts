82e61c326790f9ac02ebfdd0d735ea988f0fb426
Stefano Nebo <30480337+snebo@users.noreply.github.com>
2026-01-30 20:43:35 +0100

    feat: include array properties to creation form

--- src/app/components/product-form/product-form.ts
+++ src/app/components/product-form/product-form.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  ReactiveFormsModule,
+  Validators,
+  FormGroup,
+  FormArray,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ProductService } from '../../service/product';
 import { Router } from '@angular/router';
@@ -27,7 +33,29 @@ export class ProductFormComponent {
       imageUrl: ['', [Validators.required, Validators.pattern('https?://.+')]],
       inStock: [false],
       rating: [0, [Validators.min(0), Validators.max(5)]],
+      properties: this.fb.array([this.createProperty()]),
     });
+  }
+
+  createProperty(): FormGroup {
+    return this.fb.group({
+      color: ['', Validators.required],
+      weight: ['', Validators.required],
+    });
+  }
+
+  get properties(): FormArray {
+    return this.productForm.get('properties') as FormArray;
+  }
+
+  addProperty(): void {
+    this.properties.push(this.createProperty());
+  }
+
+  removeProperty(index: number): void {
+    if (this.properties.length > 1) {
+      this.properties.removeAt(index);
+    }
   }
 
   onSubmit(): void {
@@ -45,4 +73,5 @@ export class ProductFormComponent {
     }
   }
 }
+
 
